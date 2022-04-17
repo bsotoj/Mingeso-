646ad396dc3f39436b3046895643ca7a3bb36d98
bsotoj <bastian.soto.j@usach.cl>
2022-04-16 22:02:08 -0400

    Creación proyecto en springboot

---
+++ src/main/java/com/example/mingeso/mostrarMensaje.java
@@ -0,0 +1,12 @@
+package com.example.mingeso;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class mostrarMensaje {
+    @GetMapping("/mensaje")
+        public String mensaje(){
+        return("Aplicación web muy simple");
+        }
+}
---
+++ src/test/java/com/example/mingeso/MingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingesoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
